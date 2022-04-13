cdf51173469e24d440536373f47f2eb6c9dbfca5
JonScottE <91569353+JonScottE@users.noreply.github.com>
2022-04-12 22:01:35 -0500

    Add files via upload

--- contracts...fundrop.sol
+++ contracts...fundrop.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0; // newest, end of Saturday class
+pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
