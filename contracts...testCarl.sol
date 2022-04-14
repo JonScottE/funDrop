b57a3e009cc8d3116485b7c4c4e97efd83bcef08
JonScottE <91569353+JonScottE@users.noreply.github.com>
2022-04-14 15:41:43 -0500

    Add files via upload

--- contracts...testCarl.sol
+++ contracts...testCarl.sol
@@ -12,13 +12,10 @@ contract FUNdrop is ERC721Full {  //makes the contract take the import above
         uint256 donation_amount; // for declaring a donation amount
         
     }
-    struct Donor {
-    uint totalDonation;
-    bool isEntity;
-  }
+    
 
    mapping(uint256 => FUNdraiser) public FUNdraiser_mapping; // Saves info defined below to the struct above
-   mapping (address => Donor) public Donors; //saves the 
+   
 
     event Donation(uint256 tokenId, uint256 donation_amount, string reportURI); // when someone makes a donation, needs a ID , amount (although
 
@@ -38,6 +35,42 @@ contract FUNdrop is ERC721Full {  //makes the contract take the import above
         _setTokenURI(tokenId, tokenURI);
 
         FUNdraiser_mapping[tokenId] = FUNdraiser(owner, cause, donation_amount); //donor_wallet, was removed, this defines what is being mapped
-
         return tokenId;
     }
+
+    // Donation function -  whelp... at least I wrote it 
+    
+
+    function donate(
+        address payable owner
+        ) public payable returns (bool success) {
+        owner.transfer(msg.value);
+        return true;
+        }
+
+
+}
+/* 
+    struct Donor {
+        uint totalDonation;
+        bool isEntity;
+        
+    }
+    mapping (address => Donor) public Donors; //saves the info FROM SOMETHING THAT STILL NEEDS TO BE MADE
+
+
+    address payable sender;
+    uint i;
+    address payable[]  entries;
+    
+        function donate() public payable returns(bool success) {
+        sender = msg.sender;
+        Donors[sender].totalDonation += msg.value;
+        i = 0;
+    for (i=0; i<msg.value; i+=10000000000000000) {
+      entries.push(sender);
+    }
+    return true;
+    }
+
+        */
