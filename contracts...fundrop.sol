b424dc8e6fbd8224bb0e438f58b8601421290e58
JonScottE <91569353+JonScottE@users.noreply.github.com>
2022-04-15 11:33:55 -0500

    Add files via upload

--- contracts...fundrop.sol
+++ contracts...fundrop.sol
@@ -1,52 +1,48 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.0; // newest, end of Tuesday class
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
-contract FUNdrop is ERC721Full {
-    constructor() public ERC721Full("FUNdropToken", "DROP") {}
+contract FUNdrop is ERC721Full {  //makes the contract take the import above
+    constructor() public ERC721Full("FUNdropToken", "DROP") {} //
 
     struct FUNdraiser{  // this is creating the NFT- which we are treating as the "cause" or fundraising event
         address payable owner; // this is stating who the funds from the event will go to
         string cause; // creating the name of the fundraising event
-        address donor_wallet; // taking the ID/wallet of the donors
         uint256 donation_amount; // for declaring a donation amount
         
     }
+    
 
-    mapping(uint256 => FUNdraiser) public FUNdraiser_mapping; // ???? NOT REALLY SURE
+   mapping(uint256 => FUNdraiser) public FUNdraiser_mapping; // Saves info defined below to the struct above
+   
 
     event Donation(uint256 tokenId, uint256 donation_amount, string reportURI); // when someone makes a donation, needs a ID , amount (although
 
-    function CreateFundraiser(
-        string memory cause,
-        address payable owner,
-        address donor_wallet,
-        uint256 donation_amount,
-        string memory tokenURI
+    function CreateFundraiser( // allows someone to initiate the struct
+        string memory cause, //states and saves user input for cause
+        address payable owner, // states that owner is payable
+        uint256 donation_amount, // states user input as donation amount
+        string memory tokenURI  // creates a unique identifier
         
-    ) public returns (uint256) {
-        uint256 tokenId = totalSupply();
+        
+    ) public returns (uint256) { //public so anyone can do it, and returns a number greater than 0
+        uint256 tokenId = totalSupply(); //every new token ID is added to the total supply
 
     
 
-        _mint(owner, tokenId);
+        _mint(owner, tokenId); //makes the NFT and 
         _setTokenURI(tokenId, tokenURI);
 
-        FUNdraiser_mapping[tokenId] = FUNdraiser(owner, cause, donor_wallet, donation_amount);
-
+        FUNdraiser_mapping[tokenId] = FUNdraiser(owner, cause, donation_amount); //donor_wallet, was removed, this defines what is being mapped
         return tokenId;
     }
-/*
-    function newAppraisal(
-        uint256 tokenId,
-        uint256 newAppraisalValue,
-        string memory reportURI
-    ) public returns (uint256) {
-        artCollection[tokenId].appraisalValue = newAppraisalValue;
 
-        emit Appraisal(tokenId, newAppraisalValue, reportURI);
+    // Donation function -   
+function donate(
+        
+        ) public payable returns (bool success) {
+        return true;
+        }
+    
 
-        return artCollection[tokenId].appraisalValue;
-    }
-    */
 }
